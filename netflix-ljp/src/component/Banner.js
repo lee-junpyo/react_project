26ecbdb4527ced95a5f5506a7a73d5e9e6974fe8
ljp <2junticket@gmail.com>
2023-03-21 18:33:00 +0900

    홈페이지 배너 만들기 완료

---
+++ netflix-ljp/src/component/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+//배너에는 popularMovies의 첫번째 영화
+const Banner = ({movie}) => {
+    console.log('movie',movie)
+  return (
+    <div className='banner' style={{
+        backgroundImage : "url("+
+        `https://www.themoviedb.org/t/p/w440_and_h660_face/${movie.poster_path}`+ 
+        ")"
+    }}>
+    <div className='banner-info'>
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+    </div>
+    </div>
+  )
+}
+
+export default Banner
--- netflix-ljp/src/pages/Home.js
+++ netflix-ljp/src/pages/Home.js
@@ -1,13 +1,29 @@
 import React, { useEffect } from 'react'
 import { movieAction } from '../redux/action/movieAction';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import Banner from '../component/Banner';
+import MovieSlide from '../component/MovieSlide';
 const Home = () => {
     const dispatch = useDispatch();
+    const {popularMovies, topRatedMovies, upcomingMovies} = useSelector(state=>state.movie);
+    //console.log('home', popularMovies);
     useEffect(()=> {
         dispatch(movieAction.getMovies());
     },[]);
   return (
-    <div>Home</div>
+    <div>
+        {/* 조건부 렌더링 */}
+        {popularMovies.results && 
+            (<Banner movie={popularMovies.results[1]} />)
+        }
+        <h1>인기 많은 영화(Popular Movie)</h1>
+        <MovieSlide movie={popularMovies} />
+        <h1>평점 좋은 영화(Top rated Movie)</h1>
+        <MovieSlide movie={topRatedMovies} />
+        <h1>개봉 예정 영화(Upcoming Movie)</h1>
+        <MovieSlide movie={upcomingMovies} />
+    </div>
   )
 }
 
